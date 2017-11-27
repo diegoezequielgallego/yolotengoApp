9e64fba4e6df837514f9d64905240bd93afec29d
TECNO\dgallego <dgallego@tecnosoftware.com>
2017-11-27 16:42:34 -0300

    Add dtos

---
+++ commonLibApp/src/main/java/com/yolotengo/commonLibApp/dto/ItemDTO.java
@@ -0,0 +1,23 @@
+package com.yolotengo.commonLibApp.dto;
+
+/**
+ * Created by dgallego on 27/11/2017.
+ */
+public class ItemDTO {
+
+    private String name;
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
